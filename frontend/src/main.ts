6939e74148a37897a847f9967022961fa40d9027
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-23 11:07:05 +0200

    Added tasks

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+//Step 1: Create an array of numbers. Use the '.map' function to double all the numbers in the array.
+let myNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(myNumbers);
+myNumbers = myNumbers.map((x: number) => x * 2);
+console.log(myNumbers);
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Step 2: Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
+let myWords: string[] = ["Hallo", "das", "ist", "ein", "Dinosaurier"];
+console.log(myWords);
+myWords = myWords.filter((w: string): boolean => w.length > 5);
+console.log(myWords);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Step 3: Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
+let sum: number = myNumbers.reduce((previousValue: number, currentValue: number): number => previousValue + currentValue)
+console.log(sum)
+
+// Step 4: Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
+let isGreaterThan10: boolean = myNumbers.some((value: number): boolean => value > 10);
+console.log(isGreaterThan10);
